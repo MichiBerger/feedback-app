7323b57dbc9c5bee7412d7c1612a4ae7644eb7a7
Michi Berger <mberger83@yahoo.de>
2022-02-17 21:04:14 +0100

    Add 6. Conditionals in JSX

--- src/App.js
+++ src/App.js
@@ -7,18 +7,28 @@ export default function App() {
     { id: 3, text: "Comment three" },
   ];
 
+  const loading = false;
+  const showComments = true;
+
+  if (loading) return <h1>Loading...</h1>;
+
+  const commentBlock = (
+    <div>
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={comment.id}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
   return (
     <div className="container">
       <h1>{title.toUpperCase()}</h1>
       <p>{body}</p>
-      <div>
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={comment.id}>{comment.text}</li>
-          ))}
-        </ul>
-      </div>
+
+      {showComments && commentBlock}
     </div>
   );
 }
